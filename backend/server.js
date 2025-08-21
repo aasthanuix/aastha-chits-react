ef4b40bce021d79ded1103f247c831cabba86118
aasthanuix <digital.aasthanuix@gmail.com>
2025-08-21 12:44:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -81,7 +81,7 @@ app.use(cors({
       return callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ['Content-Type','Authorization'],
   credentials: true
 }));
