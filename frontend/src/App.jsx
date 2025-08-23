fe9a8a174e6e70b6faac10fe5fc7ae926add248e
aasthanuix <digital.aasthanuix@gmail.com>
2025-08-23 15:30:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ import ResetPassword from './pages/ForgotPassword/ResetPassword';
 
 const App = () => {
 
-  const url = "https://aasthachits-backend.onrender.com";
+  const url = "https://admin.aasthageooads.com;
 
   return (
     <Routes>
