dbb4496210ee9359c7c364e140f8a71728f3a35b
aasthanuix <digital.aasthanuix@gmail.com>
2025-09-02 17:10:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 const server = http.createServer(app);
 
 
-const PORT = process.env.PORT || 6000; // Render will inject PORT
+const PORT = process.env.PORT || 6000; 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
