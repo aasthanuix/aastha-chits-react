0d1a7c313e468621b18a9c489e7b949c9af14a51
aasthanuix <digital.aasthanuix@gmail.com>
2026-01-08 17:39:29 +0530

    Update brochureModel.js

--- backend/models/brochureModel.js
+++ backend/models/brochureModel.js
@@ -1,1 +1,11 @@
+// models/Brochure.js
+import mongoose from "mongoose";
+
+const brochureSchema = new mongoose.Schema({
+  title: String,
+  fileUrl: String, // Cloudinary secure_url
+  uploadedAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Brochure", brochureSchema);
 
