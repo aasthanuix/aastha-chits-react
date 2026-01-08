1d1291a6e1ec0a5e07cb678a5b5aee7fa8822961
aasthanuix <digital.aasthanuix@gmail.com>
2026-01-08 17:32:39 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,1 +1,15 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config(); // make sure env vars are loaded
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+console.log("Cloudinary API Key:", process.env.CLOUDINARY_API_KEY); // temporary check
+
+export default cloudinary;
 
