efe77bd049375dd44d927af7e75d3f068dc9c0b3
aasthanuix <digital.aasthanuix@gmail.com>
2026-01-08 17:42:22 +0530

    Update brochureRoute.js

--- backend/routes/brochureRoute.js
+++ backend/routes/brochureRoute.js
@@ -1,1 +1,14 @@
+import express from 'express';
+import { uploadBrochure, downloadBrochure } from '../controllers/brochureController.js';
+import  upload  from '../middlewares/upload.js';
+
+const router = express.Router();
+
+console.log('Brochure routes loaded!');
+
+// Upload brochure
+router.post('/admin/upload-brochure', upload.single('brochure'), uploadBrochure);
+router.post('/send-brochure', downloadBrochure)
+
+export default router;
 
