96144bc85d544dee54f57feea617ef73138e3d3d
aasthanuix <digital.aasthanuix@gmail.com>
2026-01-08 18:10:16 +0530

    Update enrollRoute.js

--- backend/routes/enrollRoute.js
+++ backend/routes/enrollRoute.js
@@ -1,1 +1,9 @@
+import express from "express";
+import { sendEnrollMail } from "../controllers/enrollController.js";
+
+const router = express.Router();
+
+router.post('/send-email', sendEnrollMail);
+
+export default router;
 
